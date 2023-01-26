a61d5ba56ce32c02ea92c37bdf82338bba04ac49
El Mehdi KENANE <appdevelopperpro@gmail.com>
2023-01-26 02:52:08 +0100

    new_list function

--- functions/new_list.c
+++ functions/new_list.c
@@ -6,7 +6,7 @@
 /*   By: ekenane <ekenane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 23:43:50 by ekenane           #+#    #+#             */
-/*   Updated: 2023/01/26 01:06:04 by ekenane          ###   ########.fr       */
+/*   Updated: 2023/01/26 02:51:46 by ekenane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Node *new_list(int value)
 	head = calloc(1, sizeof(Node));
 	if (!head)
 	{
-		printf("\033[37;41mthe list DOESN'T created!!!\033[0m\n");
+		printf("\033[37;41mthe list DOESN'T created!!!\n\033[0m");
 		return (NULL);
 	}
 	head->value = value;
